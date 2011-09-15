4a2b0d0ef05f9fee3baabdcf8514d993b41c2277
Robert Niederreiter <rnix@squarewave.at>
2011-09-15 08:14:15 +0200

    prepare internal preview release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9dev'
+version = '0.9pre2'
 shortdesc = 'Handle web application parts as tiles.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc = open(os.path.join(os.path.dirname(__file__), 'src', 'cone', 'tile',
